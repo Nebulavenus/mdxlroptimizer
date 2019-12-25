148a97e2da00ccf88e3a1ffb0a05a9f311eec165
Nebula Venus <6595784+Nebulavenus@users.noreply.github.com>
2019-12-25 14:27:40 +0600

    New chunks parsed: GLBS, TXAN. Flags: KTAT, KTAR, KTAS.

--- src/model.rs
+++ src/model.rs
@@ -5,8 +5,14 @@ const MDLX_TAG: u32 = 1481393229;
 const VERS_TAG: u32 = 1397900630;
 const MODL_TAG: u32 = 1279545165;
 const SEQS_TAG: u32 = 1397835091;
+const GLBS_TAG: u32 = 1396853831;
 const TEXS_TAG: u32 = 1398293844;
+const TXAN_TAG: u32 = 1312905300;
 const PIVT_TAG: u32 = 1414941008;
+
+const KTAT_TAG: u32 = 1413567563;
+const KTAR_TAG: u32 = 1380013131;
+const KTAS_TAG: u32 = 1396790347;
 
 pub fn read_mdx_file(data: Vec<u8>) {
     let offset = &mut 0usize;
@@ -47,10 +53,18 @@ fn handle_tag(tag: u32, data: &[u8], offset: &mut usize) -> Result<(), scroll::E
             let sequence_chunk = data.gread_with::<SequenceChunk>(offset, LE)?;
             dbg!(sequence_chunk);
         },
+        GLBS_TAG => {
+            let global_sequence_chunk = data.gread_with::<GlobalSequenceChunk>(offset, LE)?;
+            dbg!(global_sequence_chunk);
+        }
         TEXS_TAG => {
             let texture_chunk = data.gread_with::<TextureChunk>(offset, LE)?;
             dbg!(texture_chunk);
         },
+        TXAN_TAG => {
+            let texture_animation_chunk = data.gread_with::<TextureAnimationChunk>(offset, LE)?;
+            dbg!(texture_animation_chunk);
+        }
         PIVT_TAG => {
             let pivot_chunk = data.gread_with::<PivotPointChunk>(offset, LE)?;
             dbg!(pivot_chunk);
@@ -221,16 +235,51 @@ impl ctx::TryFromCtx<'_, Endian> for Sequence {
 
 #[derive(PartialEq, Debug)]
 pub struct GlobalSequenceChunk {
-    pub glbs: u32,
     pub chunk_size: u32,
 
     // chunk_size / 4
     pub data: Vec<GlobalSequence>,
 }
 
+impl ctx::TryFromCtx<'_, Endian> for GlobalSequenceChunk {
+    type Error = scroll::Error;
+
+    fn try_from_ctx(src: &[u8], ctx: Endian) -> Result<(Self, usize), Self::Error> {
+        let offset = &mut 0;
+        let chunk_size = src.gread_with::<u32>(offset, ctx)?;
+
+        let mut data = Vec::new();
+        if let Some(sequence_count) = u32::checked_div(chunk_size.clone(), 4) {
+            for _ in 0..sequence_count {
+                let sequence = src.gread_with::<GlobalSequence>(offset, ctx)?;
+                data.push(sequence);
+            }
+        }
+
+        Ok((GlobalSequenceChunk {
+            chunk_size,
+            data,
+        }, *offset))
+    }
+}
+
 #[derive(PartialEq, Debug)]
 pub struct GlobalSequence {
     pub duration: u32,
+}
+
+impl ctx::TryFromCtx<'_, Endian> for GlobalSequence {
+    type Error = scroll::Error;
+
+    fn try_from_ctx(src: &[u8], ctx: Endian) -> Result<(Self, usize), Self::Error> {
+        let offset = &mut 0;
+
+        let duration = src.gread_with::<u32>(offset, ctx)?;
+
+        Ok((GlobalSequence {
+            duration,
+        }, *offset))
+    }
 }
 
 #[derive(PartialEq, Debug)]
@@ -297,27 +346,133 @@ impl ctx::TryFromCtx<'_, Endian> for Texture {
 
 #[derive(PartialEq, Debug)]
 pub struct TextureAnimationChunk {
-    pub txan: u32,
     pub chunk_size: u32,
 
     pub data: Vec<TextureAnimation>,
 }
 
+impl ctx::TryFromCtx<'_, Endian> for TextureAnimationChunk {
+    type Error = scroll::Error;
+
+    fn try_from_ctx(src: &[u8], ctx: Endian) -> Result<(Self, usize), Self::Error> {
+        let offset = &mut 0;
+        let chunk_size = src.gread_with::<u32>(offset, ctx)?;
+
+        let mut data = Vec::new();
+        let mut total_size = 0u32;
+        while total_size < chunk_size {
+            let texture_animation = src.gread_with::<TextureAnimation>(offset, ctx)?;
+            total_size += texture_animation.inclusive_size;
+            data.push(texture_animation);
+        }
+
+        Ok((TextureAnimationChunk {
+            chunk_size,
+            data,
+        }, *offset))
+    }
+}
+
 #[derive(PartialEq, Debug)]
 pub struct TextureAnimation {
     pub inclusive_size: u32,
+
+    pub texture_translation: Option<TextureTranslation>,
+    pub texture_rotation: Option<TextureRotation>,
+    pub texture_scaling: Option<TextureScaling>,
+}
+
+impl ctx::TryFromCtx<'_, Endian> for TextureAnimation {
+    type Error = scroll::Error;
+
+    fn try_from_ctx(src: &[u8], ctx: Endian) -> Result<(Self, usize), Self::Error> {
+        let offset = &mut 0;
+
+        let inclusive_size = src.gread_with::<u32>(offset, ctx).unwrap();
+        let mut texture_animation = TextureAnimation {
+            inclusive_size,
+            texture_translation: None,
+            texture_rotation: None,
+            texture_scaling: None
+        };
+
+        while (*offset as u32) < inclusive_size {
+            let tag = src.gread_with::<u32>(offset, ctx).unwrap();
+            dbg!(format!("{:X}", &tag));
+            dbg!(&tag);
+
+            match tag {
+                KTAT_TAG => {
+                    let ktat = src.gread_with::<TextureTranslation>(offset, ctx)?;
+                    //dbg!(&ktat);
+                    texture_animation.texture_translation = Some(ktat);
+                },
+                KTAR_TAG => {
+                    let ktar = src.gread_with::<TextureRotation>(offset, ctx)?;
+                    //dbg!(&ktar);
+                    texture_animation.texture_rotation = Some(ktar);
+                },
+                KTAS_TAG => {
+                    let ktas = src.gread_with::<TextureScaling>(offset, ctx)?;
+                    //dbg!(&ktas);
+                    texture_animation.texture_scaling = Some(ktas);
+                },
+                _ => unreachable!(),
+            }
+        }
+
+        Ok((texture_animation, *offset))
+    }
 }
 
 #[derive(PartialEq, Debug)]
 pub struct TextureTranslation {
-    pub ktat: u32,
-
     pub number_of_tracks: u32,
     pub interpolation_type: u32,
     pub global_sequence_id: u32,
 
     // number_of_tracks
     pub data: Vec<TranslationTrack>,
+}
+
+impl ctx::TryFromCtx<'_, Endian> for TextureTranslation {
+    type Error = scroll::Error;
+
+    fn try_from_ctx(src: &[u8], ctx: Endian) -> Result<(Self, usize), Self::Error> {
+        let offset = &mut 0;
+
+        let number_of_tracks = src.gread_with::<u32>(offset, ctx)?;
+        let interpolation_type = src.gread_with::<u32>(offset, ctx)?;
+        let global_sequence_id = src.gread_with::<u32>(offset, ctx)?;
+
+        let mut data = Vec::new();
+        for _ in 0..number_of_tracks {
+            let mut translation_track = src.gread_with::<TranslationTrack>(offset, ctx)?;
+            if interpolation_type > 1 {
+                let in_tan = [
+                    src.gread_with::<f32>(offset, ctx)?,
+                    src.gread_with::<f32>(offset, ctx)?,
+                    src.gread_with::<f32>(offset, ctx)?,
+                ];
+                let out_tan = [
+                    src.gread_with::<f32>(offset, ctx)?,
+                    src.gread_with::<f32>(offset, ctx)?,
+                    src.gread_with::<f32>(offset, ctx)?,
+                ];
+
+                translation_track.in_tan = Some(in_tan);
+                translation_track.out_tan = Some(out_tan);
+            }
+            data.push(translation_track);
+        }
+
+        Ok((TextureTranslation {
+            number_of_tracks,
+            interpolation_type,
+            global_sequence_id,
+            data,
+        }, *offset))
+    }
 }
 
 #[derive(PartialEq, Debug)]
@@ -328,16 +483,78 @@ pub struct TranslationTrack {
     pub out_tan: Option<[f32; 3]>,
 }
 
+impl ctx::TryFromCtx<'_, Endian> for TranslationTrack {
+    type Error = scroll::Error;
+
+    fn try_from_ctx(src: &[u8], ctx: Endian) -> Result<(Self, usize), Self::Error> {
+        let offset = &mut 0;
+
+        let time = src.gread_with::<u32>(offset, ctx)?;
+        let translation = [
+            src.gread_with::<f32>(offset, ctx)?,
+            src.gread_with::<f32>(offset, ctx)?,
+            src.gread_with::<f32>(offset, ctx)?,
+        ];
+
+        Ok((TranslationTrack {
+            time,
+            translation,
+            in_tan: None,
+            out_tan: None
+        }, *offset))
+    }
+}
+
 #[derive(PartialEq, Debug)]
 pub struct TextureRotation {
-    pub ktar: u32,
-
     pub number_of_tracks: u32,
     pub interpolation_type: u32,
     pub global_sequence_id: u32,
 
     // number_of_tracks
     pub data: Vec<RotationTrack>,
+}
+
+impl ctx::TryFromCtx<'_, Endian> for TextureRotation {
+    type Error = scroll::Error;
+
+    fn try_from_ctx(src: &[u8], ctx: Endian) -> Result<(Self, usize), Self::Error> {
+        let offset = &mut 0;
+
+        let number_of_tracks = src.gread_with::<u32>(offset, ctx)?;
+        let interpolation_type = src.gread_with::<u32>(offset, ctx)?;
+        let global_sequence_id = src.gread_with::<u32>(offset, ctx)?;
+
+        let mut data = Vec::new();
+        for _ in 0..number_of_tracks {
+            let mut rotation_track = src.gread_with::<RotationTrack>(offset, ctx)?;
+            if interpolation_type > 1 {
+                let in_tan = [
+                    src.gread_with::<f32>(offset, ctx)?,
+                    src.gread_with::<f32>(offset, ctx)?,
+                    src.gread_with::<f32>(offset, ctx)?,
+                    src.gread_with::<f32>(offset, ctx)?,
+                ];
+                let out_tan = [
+                    src.gread_with::<f32>(offset, ctx)?,
+                    src.gread_with::<f32>(offset, ctx)?,
+                    src.gread_with::<f32>(offset, ctx)?,
+                    src.gread_with::<f32>(offset, ctx)?,
+                ];
+
+                rotation_track.in_tan = Some(in_tan);
+                rotation_track.out_tan = Some(out_tan);
+            }
+            data.push(rotation_track);
+        }
+
+        Ok((TextureRotation {
+            number_of_tracks,
+            interpolation_type,
+            global_sequence_id,
+            data,
+        }, *offset))
+    }
 }
 
 #[derive(PartialEq, Debug)]
@@ -348,10 +565,31 @@ pub struct RotationTrack {
     pub out_tan: Option<[f32; 4]>,
 }
 
+impl ctx::TryFromCtx<'_, Endian> for RotationTrack {
+    type Error = scroll::Error;
+
+    fn try_from_ctx(src: &[u8], ctx: Endian) -> Result<(Self, usize), Self::Error> {
+        let offset = &mut 0;
+
+        let time = src.gread_with::<u32>(offset, ctx)?;
+        let rotation = [
+            src.gread_with::<f32>(offset, ctx)?,
+            src.gread_with::<f32>(offset, ctx)?,
+            src.gread_with::<f32>(offset, ctx)?,
+            src.gread_with::<f32>(offset, ctx)?,
+        ];
+
+        Ok((RotationTrack {
+            time,
+            rotation,
+            in_tan: None,
+            out_tan: None
+        }, *offset))
+    }
+}
+
 #[derive(PartialEq, Debug)]
 pub struct TextureScaling {
-    pub ktas: u32,
-
     pub number_of_tracks: u32,
     pub interpolation_type: u32,
     pub global_sequence_id: u32,
@@ -360,12 +598,74 @@ pub struct TextureScaling {
     pub data: Vec<ScalingTrack>,
 }
 
+impl ctx::TryFromCtx<'_, Endian> for TextureScaling {
+    type Error = scroll::Error;
+
+    fn try_from_ctx(src: &[u8], ctx: Endian) -> Result<(Self, usize), Self::Error> {
+        let offset = &mut 0;
+
+        let number_of_tracks = src.gread_with::<u32>(offset, ctx)?;
+        let interpolation_type = src.gread_with::<u32>(offset, ctx)?;
+        let global_sequence_id = src.gread_with::<u32>(offset, ctx)?;
+
+        let mut data = Vec::new();
+        for _ in 0..number_of_tracks {
+            let mut scaling_track = src.gread_with::<ScalingTrack>(offset, ctx)?;
+            if interpolation_type > 1 {
+                let in_tan = [
+                    src.gread_with::<f32>(offset, ctx)?,
+                    src.gread_with::<f32>(offset, ctx)?,
+                    src.gread_with::<f32>(offset, ctx)?,
+                ];
+                let out_tan = [
+                    src.gread_with::<f32>(offset, ctx)?,
+                    src.gread_with::<f32>(offset, ctx)?,
+                    src.gread_with::<f32>(offset, ctx)?,
+                ];
+
+                scaling_track.in_tan = Some(in_tan);
+                scaling_track.out_tan = Some(out_tan);
+            }
+            data.push(scaling_track);
+        }
+
+        Ok((TextureScaling {
+            number_of_tracks,
+            interpolation_type,
+            global_sequence_id,
+            data,
+        }, *offset))
+    }
+}
+
 #[derive(PartialEq, Debug)]
 pub struct ScalingTrack {
     pub time: u32,
-    pub translation: [f32; 3],
+    pub scaling: [f32; 3],
     pub in_tan: Option<[f32; 3]>,
     pub out_tan: Option<[f32; 3]>,
+}
+
+impl ctx::TryFromCtx<'_, Endian> for ScalingTrack {
+    type Error = scroll::Error;
+
+    fn try_from_ctx(src: &[u8], ctx: Endian) -> Result<(Self, usize), Self::Error> {
+        let offset = &mut 0;
+
+        let time = src.gread_with::<u32>(offset, ctx)?;
+        let scaling = [
+            src.gread_with::<f32>(offset, ctx)?,
+            src.gread_with::<f32>(offset, ctx)?,
+            src.gread_with::<f32>(offset, ctx)?,
+        ];
+
+        Ok((ScalingTrack {
+            time,
+            scaling,
+            in_tan: None,
+            out_tan: None
+        }, *offset))
+    }
 }
 
 #[derive(PartialEq, Debug)]
